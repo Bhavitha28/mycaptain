1cddcd267669a47f9411286c96f0c62bd65440cd
Bhavitha28 <63496208+Bhavitha28@users.noreply.github.com>
2020-04-13 15:44:18 +0530

    Create Switch.c

---
+++ Switch.c
@@ -0,0 +1,24 @@
+#include<studio.h>
+Main()
+{
+Printf("enter a number");
+Scanf("%d",&a);
+Switch(a)
+{
+Case 1:printf("Food item:pizza\n");
+       Printf("price:239");
+       Break;
+Case 2:printf("Food item:Burger\n");
+       Printf("price:129");
+       Break;
+Case 3:printf("Food item:Pasta\n");
+       Printf("price:179");
+       Break;
+Case 4:printf("Food item:French fries\n");
+       Printf("price:99");
+       Break;
+Case 5:printf("Food item:sandwich\n");
+       Printf("price:149");
+       Break;
+Case 6:default;
+}
