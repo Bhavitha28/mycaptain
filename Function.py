2da651b5c3570a46a6ebd42e15f24fdd133924dd
Bhavitha28 <63496208+Bhavitha28@users.noreply.github.com>
2020-06-04 09:03:11 +0530

    Create Function.py

---
+++ Function.py
@@ -0,0 +1,20 @@
+Def most_frequent(test_str):
+    
+    all_freq = {} 
+
+  
+
+    for i in test_str: 
+
+        if i in all_freq: 
+
+           all_freq[i] += 1
+
+        else: 
+
+            all_freq[i] = 1
+
+Most_frequent(mississipi)
+print ("Count of all characters in GeeksforGeeks is :\n "
+
+                                      
